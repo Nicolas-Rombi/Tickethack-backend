587307e3d76a1414565a19751143b75117f2c2cb
Zoé <gilzoe7@gmail.com>
2024-09-24 16:56:34 +0200

    modification backend

--- routes/index.js
+++ routes/index.js
@@ -21,14 +21,32 @@ router.post('/search', async function(req, res, next) {
 });
 
 
-router.get('./cart', async (req, res) => {
+router.get('/cart', async (req, res) => {
   try { const trips = await Trip.find();
   if (trips && trips.length > 0);
     res.json(trips);
   } catch (err) { 
-   res.status(500).send('Erreur lors de la recuperation des trajets');
+   res.status(500).send('Erreur lors de la récupération des trajets');
    };
 });
-  
+
+router.delete('/cart/:id', async (req, res) => {
+  try {
+    // Recherche et supprime le trajet par son ID
+    const trip = await Trip.findByIdAndDelete(req.params.id);
+    
+    // Vérifie si le trajet a été trouvé et supprimé
+    if (trip) {
+      res.status(204).send(); // Trajet supprimé avec succès
+    } else {
+      res.status(404).send('Trajet non trouvé'); // Trajet non existant
+    }
+  } catch (err) {
+    // Gère les erreurs lors de la suppression
+    console.error(err);
+    res.status(500).send('Erreur lors de la suppression du trajet'); // Erreur serveur
+  }
+});
+
   
 module.exports = router;
