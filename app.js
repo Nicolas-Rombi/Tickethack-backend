d67ede49e87f8f29c3d4613b6c307f1983bb81b6
Nicolas Rombi <nico461@hotmail.fr>
2024-09-24 16:51:17 +0200

    cors

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ require('./models/connection')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const cors = require('cors');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
